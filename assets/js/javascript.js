dfb5b4e4af7a25031bf931660c5e9e895e808b47
Diana Maria Silva Valencia <diana.silva.v94@gmail.com>
2017-07-13 17:10:49 -0500

    creando repositorio

---
+++ assets/js/javascript.js
@@ -0,0 +1,10 @@
+function lista (nombre){
+  var respuesta;
+  var arr = [];
+  for (nombre in nombres){
+  document.write(nombres[nombre]+"</br>");
+  } 
+
+  nombres.push(arr);
+  document.getElementById("listaFrutas").innerHTML = "";
+}
